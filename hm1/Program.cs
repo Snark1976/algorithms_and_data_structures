a331a9fd5d3fc672315752d2714c494da1a869f8
Виктор Коховец <kohovec@gmail.com>
2023-05-20 18:32:44 +0300

    hm1 complete

---
+++ hm1/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] array = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+Console.WriteLine($"[{string.Join("; ",array)}]");
+for (int i = 0, j = 0; i< array.Length; i++)
+    if (array[i] != array[j]) array[++j] = array[i];
+Console.WriteLine($"[{string.Join("; ",array)}]");
