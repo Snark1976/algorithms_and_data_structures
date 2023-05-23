f5a622f2f0f05499e0907447894bb8d73c791294
Виктор Коховец <kohovec@gmail.com>
2023-05-23 08:05:00 +0300

    complete hm2

---
+++ hm2/Program.cs
@@ -0,0 +1,11 @@
+﻿Random random = new();
+int number = 10;
+int[] array = new int[100].Select(x => random.Next(number)).ToArray(); 
+Console.WriteLine($"[{string.Join("; ",array)}]");
+int[] counters = new int[number];
+foreach (int element in array)
+    counters[element]++;
+for (int i = 0, j = 0; i < counters.Length; i++ )  
+    for (int k = 0; k < counters[i]; k++)
+    array[j++] = i;
+Console.WriteLine($"[{string.Join("; ",array)}]");
