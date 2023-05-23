a71886ac7eca91e5f62000dad9ac97ee6eb1274b
Виктор Коховец <kohovec@gmail.com>
2023-05-23 08:35:05 +0300

    added const to hm2

--- hm1/Program.cs
+++ hm1/Program.cs
@@ -1,5 +1,6 @@
 ﻿int[] array = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
 Console.WriteLine($"[{string.Join("; ",array)}]");
 for (int i = 0, j = 0; i< array.Length; i++)
-    if (array[i] != array[j]) array[++j] = array[i];
+    if (array[i] != array[j]) 
+        array[++j] = array[i];
 Console.WriteLine($"[{string.Join("; ",array)}]");
--- hm2/Program.cs
+++ hm2/Program.cs
@@ -1,11 +1,16 @@
 ﻿Random random = new();
-int number = 10;
-int[] array = new int[100].Select(x => random.Next(number)).ToArray(); 
+const int NUMBER = 10;
+int[] array = new int[100].Select(x => random.Next(NUMBER)).ToArray(); 
 Console.WriteLine($"[{string.Join("; ",array)}]");
-int[] counters = new int[number];
-foreach (int element in array)
-    counters[element]++;
-for (int i = 0, j = 0; i < counters.Length; i++ )  
-    for (int k = 0; k < counters[i]; k++)
-    array[j++] = i;
+CountingSort(array);
 Console.WriteLine($"[{string.Join("; ",array)}]");
+
+void CountingSort(int[] array, int number = NUMBER) 
+{
+    int[] counters = new int[number];
+    foreach (int element in array)
+        counters[element]++;
+    for (int i = 0, j = 0; i < counters.Length; i++ )  
+        for (int k = 0; k < counters[i]; k++)
+        array[j++] = i;
+}
